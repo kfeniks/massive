29013d9b7b6ace977858597b25396a984ee19897
Alexey Dubinin <keccgroup@gmail.com>
2017-07-22 23:33:10 +0300

    New lesson add about function and parameters

--- index.php
+++ index.php
@@ -5,4 +5,29 @@
  * Date: 22.07.17
  * Time: 19:52
  */
+        $h1='<h1>';
+        $h1end='</h1>';
+        $text='Урок массивы';
+
+        class foo {
+            var $bar = 'I am bar.';
+            var $arr = array('I am A.', 'I am B.', 'I am C.');
+            var $r   = 'I am r.';
+        }
+
+        $array = [
+            "foo" => "bar",
+            "bar" => "foo",
+        ];
+
+        $foo = new foo();
+?>
+<?= $h1.$text.$h1end ?>
+
+<?= $array['foo']?>
+
+<?= $foo->{bar} ?>
+
+
+
 
