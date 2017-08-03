18892b6f9fab7da1cd1f8a51fdc4e9c3bd198d97
kfeniks <prinz.haruhi@gmail.com>
2017-08-03 23:17:39 +0300

    ekranirovanie

--- 02/index.php
+++ 02/index.php
@@ -9,3 +9,38 @@
 const Name = 'Banny';
 
 echo Name;
+?>
+
+<br>
+<br>
+
+<?php
+
+$a = "full";
+
+unset($a);
+
+if ($a == null) $a = 'Null';
+echo $a;
+
+?>
+
+<br>
+<br>
+
+<?php
+
+$hello = 'Hren tam >>';
+
+$a = 'hello';
+
+echo $$a;
+
+echo '<br><br>';
+
+//экранирование
+
+echo "Гость сказал: \"Всем привет!\"";
+
+?>
+
