6fa93a69403c81ccb016ebd0ee988ec61bf374a3
kfeniks <prinz.haruhi@gmail.com>
2017-08-04 14:33:41 +0300

    try make own key in simply massive

---
+++ 04/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 04.08.2017
+ * Time: 14:29
+ */
+
+$arr = array('Andy', 'Boris', 'Benny', 'Halley');
+
+print($arr[0]);
+
+echo '<br><br>';
+//make own key
+
+$arr = array('5' => 'Andy', '8' => 'Boris',
+    '11' => 'Benny', '10' => 'Halley');
+
+print($arr[11]);
